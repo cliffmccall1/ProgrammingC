3e241b217991f9889a2a043df955e1551fcb99e9
Cliff McCall <cliff.mccall1@gmail.com>
2018-11-29 13:25:15 -0600

    completed lesson10

---
+++ finalProject/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using blingBlong = game.GameProgram;
+using partTwo = myNamespaceTwo.ProgramTwo;
+
+namespace finalProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // blingBlong.play();
+            partTwo.print();
+        }
+    }
+}
